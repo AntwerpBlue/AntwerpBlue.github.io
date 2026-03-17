3c617642842a1afab353f6fa35f0fb8e9ee3158e
Oren <112540659+AntwerpBlue@users.noreply.github.com>
2026-03-17 22:45:57 +0800

    Add site search and code theme

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,5 +3,13 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://antwerpblue.github.io",
   trailingSlash: "always",
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
 });
 
